863e3a1bf618b98f24d16acc6fef0e56c48539ac
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2022-05-11 15:44:08 +0800

    f

--- projector.py
+++ projector.py
@@ -57,7 +57,10 @@ def project(
     noise_bufs = { name: buf for (name, buf) in G.synthesis.named_buffers() if 'noise_const' in name }
 
     # Load VGG16 feature detector.
-    url = 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metrics/vgg16.pt'
+    # url = 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metrics/vgg16.pt'
+    # with dnnlib.util.open_url(url) as f:
+    #     vgg16 = torch.jit.load(f).eval().to(device)
+    url = 'vgg16.pt'
     with dnnlib.util.open_url(url) as f:
         vgg16 = torch.jit.load(f).eval().to(device)
 
